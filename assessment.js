2b00550c2621b1dbb56170454a820481c6b4fe40
Aj Wietecha II <ajwietechaii@yahoo.com>
2017-09-01 12:36:34 -0500

    assessement

--- assessment.js
+++ assessment.js
@@ -1,10 +1,12 @@
 
 // #1 Create a variable called hello and assign it the string 'goodbye'
 
-
+var hello = 'goodbye';
 
 // #2 Use the variable iLove to create a new variable called iLoveCode that is assigned the string "I love code"
 var iLove = 'I love';
+
+var iLoveCode = iLove + " code";
 
 // #3  Make an object called bob and give it the following properties
 // bob has a height of 6ft (string)
@@ -12,6 +14,14 @@ var iLove = 'I love';
 // bob has hair, that has style spikey, and color brown (object)
 // bob is not presidentOfTheUnitedStates (boolean)
 // bob likes apples, bananas, and cherries (array of strings)
+//
+var bob = {
+	height: '6ft',
+	age: 24,
+	hair:{style: 'spikey', color : 'brown'},///something wrong
+	presidentOfTheUnitedStates: false,
+	likes:['apples','bananas','cherries']
+};
 
 
 // #4 Change my shirt color to pink using dot notation
@@ -20,16 +30,25 @@ var myShirt = {
 	color: 'red'
 };
 
+myShirt.color = 'pink';
+
 // Change my shirt type to spandex using square bracket notation
 var myOtherShirt = {
 	type: 'polo',
 	color: 'red'
 };
 
+myOtherShirt['type'] = 'spandex';
+
 // #5 Create an object that tracks a count of animals in a zoo.  Call it 'zoo'
 // The key should be the animal name(string) and the value should be how many there are.
 // Our zoo has 8 monkeys, 4 giraffes and 2 elephants
 
+var zoo = {
+	monkeys: 8,
+	giraffes: 4,
+	elephants: 2
+}
 
 // #6 Loop through this object and change all keys that start with the letter s to have a value of 's'
 
@@ -41,47 +60,107 @@ var snake = {
 	runs: 'legless'
 };
 
-//#7 Create an array of strings that are the 7 primary colors in the rainbow - red, orange, yellow, green, blue, indigo, violet (lower-case). Call your array rainbowColors
 
+for(var key in snake){
+	if(key[0] === "s"){
+		snake[key] = "s";
+	}
+}
+snake;
+
+
+
+
+
+
+//#7 Create an array of strings that are the 7 primary colors in the rainbow - red, orange, yellow, green, blue, indigo, violet (lower-case). Call your array rainbowColors
+var rainbowColors = ['red','orange', 'yellow', 'green','blue', 'indigo', 'violet'];
 // #8 Using this array do the following
 var heroes = ['superman', 'batman', 'flash'];
 // add 'wonderwoman' to the end
-
+heroes.push('wonderwoman')
 // remove 'superman' and store him in a variable called firstHero
-
+var firstHero = heroes.shift(0,1);
 // add 'spongebob' to the start of the array
-
+heroes.unshift('spongebob')
 // remove 'flash' from the array and store him in a variable called secondHero
-
+var secondHero = heroes.splice(2,1);
 // leave batman in the array but put a copy of him on a variable called thirdHero
 
-
-
+ var thirdHero = heroes.slice(1,2).toString();
 
 // #9 Write a function called addItem that takes in an array and an item, adds the item to
 // the array, and returns the array with the added item.
-
+function addItem(arr, item){
+	arr.push(item)
+	return arr;
+}
 
 // #10 Write a function called removeItem that takes in an array of strings, and a string.
-
 // Removes all instances of that string from the array. And return the modified array.
 // The order of the array should not be changed
+
+function removeItem ( arr, string) {
+
+   return arr.filter(function(word){
+      return word !== string
+    })
+}
+
+
+removeItem(['apples','bannanas','oranges', 'apples'],'apples');
 
 // #11 Write a function  called doubleTheFun that takes 1 parameter. It should double numbers, and
 // repeats strings. example 4->8, 2.5->5, 'Awesome'->'AwesomeAwesome'
 
-
+function doubleTheFun(num){
+    if (Number(num) || Number(num) == 0) {
+        return num * 2;
+    } else {
+        return num += num;
+    }
+}
 
 // #12 Write function getValueOfProperty that takes in an object, and the name of a property on the object
 // return the value from the object that corresponds to the property
+var me = {
+	name: "aj",
+	age: 21,
+	car: "honda"
+};
 
+var getValueOfProperty = function(obj, key){
+	return (obj[key]);
+};
+
+getValueOfProperty(me, "car");
 
 // #13 Write a function called makeChatMessage that takes in a message and author as parameters
 // and returns an object with a message, author, and timestamp, that is
 // the current time as a Date object
-
+function makeChatMessage(message,author) {
+  // Define desired object
+  var obj = {
+    message: message,
+    author: author,
+    timestamp: new Date()
+  };
+  // Return it
+  return obj;
+}
+makeChatMessage("John Smith", 'see');
 // #14 Create a function called coderTest that takes in an object that is a person. It looks to see if the person’s name is Jeremy and then changes the person object to have a property called lovesCode with a value of 10.  If their name is Brack set lovesCode to 0.  otherwise set lovesCode to 5.
-
+var coderTest = function(person){
+	for(var key in person){
+		if(person[key] === "Jeremy"){
+			person.lovesCode = 10;
+		} else if(person[key] === "Brack"){
+			person.lovesCode = 0;
+		} else {
+			person.lovesCode = 5;
+		}
+	}
+};
 
 // #15 Create a function called outside that takes in a temperature (number), a humidity(number), and a cloudiness(number), in that order. Using the following to return the correct values
 /*
@@ -92,6 +171,30 @@ var heroes = ['superman', 'batman', 'flash'];
     Otherwise - return "I love outside"
 */
 
+function outside(temp,humid,cloud){
+if(temp > 80 && humid > 40){
+	return "I'm all sweat";
+}
+else if(temp < 40 && cloud > 60){
+	return "I have icecicles"
+}
+else if(temp>80 && humid < 40 && cloud < 20){
+	return "I'm literally in the desert"
+}
+else if(temp > 80  || humid > 50 || cloud > 50){
+	return "Hmm, probably not"
+}
+else {
+return "I love outside"
+}
+}
+
+
 // #16 Create a function called callerBack that takes in a function (holla) and a string parameter(back) and invokes it(holla) with the argument string(back) + ' back'."
 // example - If I call you with 'Give it' you should invoke holla with 'Give it back'
 
+
+
+var callerBack = function(holla, back){
+	return holla(back + " back" );
+};
